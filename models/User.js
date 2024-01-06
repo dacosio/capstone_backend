1f4110b182ffc200b7e04613a3716646a021d593
Don Cosio <dacosio@gmail.com>
2024-01-06 15:40:12 -0800

    architecture

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
