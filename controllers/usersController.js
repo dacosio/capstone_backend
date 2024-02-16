218af5265ca0c2db40d0401fb7a1dc4c335f494c
yban <laubana@gmail.com>
2024-02-15 19:43:34 -0800

    add change image

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,32 @@
 const { s3UploadV3 } = require("../aws/s3");
+const User = require("../models/User");
 const Consumer = require("../models/Consumer");
 const Merchant = require("../models/Merchant");
+
+const getUser = async (req, res) => {
+    try {
+        const userId = req.id;
+
+        if (!userId) {
+            return res
+                .status(404)
+                .json({ error: "User not found. Please log in" });
+        }
+
+        const user = await User.findOne(
+            { _id: userId },
+            { password: 0 }
+        ).lean();
+
+        if (!user) {
+            return res.status(200).json({ message: "No user found" });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 const getAllConsumers = async (req, res) => {
     try {
@@ -40,9 +66,45 @@ const getAllMerchants = async (req, res) => {
     }
 };
 
+const updateImage = async (req, res) => {
+    try {
+        const userId = req.id;
+
+        if (!userId) {
+            return res
+                .status(404)
+                .json({ error: "User not found. Please log in" });
+        }
+
+        const { image } = req.body;
+
+        if (!image) {
+            return res.status(400).json({ message: "image is required" });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            $set: { image },
+        }).exec();
+
+        if (updatedUser) {
+            res.status(200).json({
+                message: `Image for the user has been updated to ${image}`,
+            });
+        } else {
+            res.status(400).json({
+                message: `Image for the user has not been updated to ${image}`,
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
 // sample uplaod controller
 const uploadFile = async (req, res) => {
     try {
+        console.log(req.files);
         const results = await s3UploadV3(req.files);
         res.json({ status: "success", results });
     } catch (error) {
@@ -51,7 +113,9 @@ const uploadFile = async (req, res) => {
 };
 
 module.exports = {
+    getUser,
     getAllConsumers,
     getAllMerchants,
+    updateImage,
     uploadFile,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,13 @@ const verifyJWT = require("../middleware/verifyJWT");
 
 router.use(verifyJWT);
 
+router.route("/user").get(usersController.getUser);
 router.route("/consumers").get(usersController.getAllConsumers);
 router.route("/merchants").get(usersController.getAllMerchants);
+router.route("/image").put(usersController.updateImage);
 
-//V3 s3 sdk sample endpoint that uses upload
+// V3 s3 sdk sample endpoint that uses upload
 router
-  .route("/upload-multi")
-  .post(uploadMulterToS3.array("image"), usersController.uploadFile);
+    .route("/upload-multi")
+    .post(uploadMulterToS3.array("image"), usersController.uploadFile);
 module.exports = router;
