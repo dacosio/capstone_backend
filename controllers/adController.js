f9fffa6963984b9dda10e0b8d3b711c26f3b71d1
dacosio <dacosio@gmail.com>
2024-02-28 15:27:45 -0800

    [For Review]ai for text (#22)
    * ai for text
    
    * added env.dev
    
    * error handler
    
    * chagned to 40

--- controllers/adController.js
+++ controllers/adController.js
@@ -1,3 +1,4 @@
+const { openai } = require("../config/openAi");
 const Ad = require("../models/Ad");
 const StripeCustomer = require("../models/StripeCustomer");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
@@ -91,7 +92,64 @@ const createAds = async (req, res) => {
     }
 };
 
+const generateAdText = async (req, res) => {
+    const { description } = req.body;
+    try {
+        if (!description) {
+            return res
+                .status(400)
+                .json({ error: "Description cannot be empty" });
+        }
+        const messages = [
+            {
+                role: "system",
+                content: `Your response should give always give a headline and a tagline for a promo in my restaurant where the
+                        headline has a maximum of 20 characters and the tagline has
+                        a maximum of 40 characters, both considers white spaces. Return the response in the following parsable JSON format:
+                    {
+                        "h": "headline",
+                        "t" "tagline" 
+                    }
+                    If you cannot provide the expected answer with the conditions above. Return the response:
+                    {
+                        "h": "Invalid Prompt",
+                        "t": "Please, keep the description within the promo ad."
+                    }
+                    `,
+            },
+            {
+                role: "user",
+                content: `${description}`,
+            },
+        ];
+
+        let content = null;
+        while (
+            !content ||
+            !content.h ||
+            !content.t ||
+            content.h.length > 20 ||
+            content.t.length > 50
+        ) {
+            const completion = await openai.chat.completions.create({
+                model: "gpt-3.5-turbo",
+                messages,
+                max_tokens: 100,
+                temperature: 1,
+            });
+
+            const messageObject = completion.choices[0].message.content;
+            content = JSON.parse(messageObject);
+        }
+
+        return res.status(200).json(content);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 module.exports = {
     getAllAds,
     createAds,
+    generateAdText,
 };
