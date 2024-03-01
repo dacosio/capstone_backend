46ca90713bb1f08e76d60d1ff92b1a1c2a54be6e
Don Cosio <dacosio@gmail.com>
2024-02-29 18:18:57 -0800

    price be for ads

--- routes/adRoutes.js
+++ routes/adRoutes.js
@@ -4,5 +4,8 @@ const adController = require("../controllers/adController");
 
 router.route("/ad").get(adController.getAllAds).post(adController.createAds);
 router.route("/ad/generate-ad").post(adController.generateAdText);
-
+router
+    .route("/ad/price")
+    .get(adController.getAdPrices)
+    .post(adController.createAdPrices);
 module.exports = router;
