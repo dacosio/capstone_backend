c8a3afe662fa467675d5d9fa477ad4b7c693dda4
yban <laubana@gmail.com>
2024-03-08 10:38:46 -0800

    add rating, change discount

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -1,63 +1,68 @@
-const Merchant = require("../models/Merchant");
 const Rating = require("../models/Rating");
-const Consumer = require("../models/Consumer");
 
 const getAllMerchantRatings = async (req, res) => {
-  try {
-    const { merchantId } = req.body;
+    try {
+        const { merchantId } = req.query;
 
-    if (!merchantId) {
-      return res.status(404).json({ error: "Merchant not found" });
+        if (!merchantId) {
+            return res.status(404).json({ error: "Merchant not found" });
+        }
+
+        // Find all ratings for the merchant and populate merchant and consumer fields
+        const ratings = await Rating.find({ merchant: merchantId })
+            .populate({
+                path: "merchant",
+                populate: { path: "user", select: "-password" },
+            })
+            .populate({
+                path: "consumer",
+                populate: {
+                    path: "user",
+                    select: "-password",
+                },
+            })
+            .lean();
+
+        if (!ratings?.length) {
+            return res.status(400).json({ error: "No rating" });
+        }
+
+        // Return the ratings
+        res.status(200).json(ratings);
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
     }
-
-    // Find all ratings for the merchant and populate merchant and consumer fields
-    const ratings = await Rating.find({ merchant: merchantId })
-      .populate({
-        path: "merchant",
-        populate: { path: "user", select: "-password" },
-      })
-      .populate({
-        path: "consumer",
-        populate: {
-          path: "user",
-          select: "-password",
-        },
-      })
-      .lean();
-
-    // Return the ratings
-    res.json({ ratings });
-  } catch (error) {
-    // Handle errors
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
 };
 
 const addRating = async (req, res) => {
-  try {
-    const { rating, comment, merchantId } = req.body;
+    try {
+        const { rating, comment, merchantId } = req.body;
 
-    if (!rating || !comment) {
-      return res
-        .status(400)
-        .json({ message: "Rating and comment are required" });
+        if (!rating || !comment) {
+            return res
+                .status(400)
+                .json({ message: "Rating and comment are required" });
+        }
+        const newRating = await Rating.create({
+            rating,
+            comment,
+            consumer: req.consumerId,
+            merchant: merchantId,
+        });
+
+        res.status(201).json({
+            message: "Rating added successfully",
+            newRating,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
     }
-    const newRating = await Rating.create({
-      rating,
-      comment,
-      consumer: req.consumerId,
-      merchant: merchantId,
-    });
-
-    res.status(201).json({ message: "Rating added successfully", newRating });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
 };
 
 module.exports = {
-  getAllMerchantRatings,
-  addRating,
+    getAllMerchantRatings,
+    addRating,
 };
