0f7bb29b203c7974c150682dd3ecc75a8adccbf3
yban <laubana@gmail.com>
2024-03-13 12:20:29 -0700

    add operatingTimes

--- models/Rating.js
+++ models/Rating.js
@@ -1,28 +1,29 @@
 const mongoose = require("mongoose");
 
 const RatingSchema = new mongoose.Schema(
-  {
-    rating: {
-      type: Number,
-      required: true,
+    {
+        rating: {
+            type: Number,
+            enum: [1, 2, 3, 4, 5],
+            required: true,
+        },
+        comment: {
+            type: String,
+        },
+        consumer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Consumer",
+            required: true,
+        },
+        merchant: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Merchant",
+            required: true,
+        },
     },
-    comment: {
-      type: String,
-    },
-    consumer: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Consumer",
-      required: true,
-    },
-    merchant: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Merchant",
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 module.exports = mongoose.model("Rating", RatingSchema);
