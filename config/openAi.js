e6885bffb2cf068d8ecb798dce7211812e58d51a
Don Cosio <dacosio@gmail.com>
2024-03-20 21:07:33 -0700

    removed clg

--- config/openAi.js
+++ config/openAi.js
@@ -3,7 +3,6 @@ const { OpenAI } = require("openai");
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
-console.log("🚀 ~ process.env.OPENAI_API_KEY:", process.env.OPENAI_API_KEY);
 
 module.exports = {
     openai,
